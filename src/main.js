cd6d0df7d1485adbe24742f4dc3a86e89f1177ae
plusmail <plus4957@gmail.com>
2025-05-21 09:53:32 +0900

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// main.js 또는 main.ts
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS 추가
+import 'bootstrap'; // 선택: JS 기능 사용 시
+
+createApp(App).mount('#app');
